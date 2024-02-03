2212e7b9c927ead0f2992513279a975806a679d1
Isaac Warren <isaacwarren25@gmail.com>
2024-02-03 11:49:48 -0500

    Added GeometricObject.java and Circle.java. Added compareTo and equals to Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,52 @@
-public class Circle {
+public class Circle
+        extends GeometricObject implements Comparable<Circle>{
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /** Return radius */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Set a new radius */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override /** Return area */
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    /** Return diameter */
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    @Override /** Return perimeter */
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    /** Print the circle info */
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated() +
+                " and the radius is " + radius);
+    }
+
+    /** Override compareTo() to evaluate radius*/
+    @Override
+    public int compareTo(Circle obj) {
+        return Double.compare(this.radius, obj.radius);
+    }
+
+    public boolean equals(Object obj) {
+        return this.getRadius() == ((Circle)obj).getRadius();
+    }
 }
